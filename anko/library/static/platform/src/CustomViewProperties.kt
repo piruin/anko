a09110a013c634423c70bb28b24f34d0a3301380
Piruin Panichphol <piruin.p@gmail.com>
2017-10-23 12:13:57 +0700

    Add drawableStart/Top/End/Bottom property also drawableLeft/Right

--- anko/library/static/platform/src/CustomViewProperties.kt
+++ anko/library/static/platform/src/CustomViewProperties.kt
@@ -17,8 +17,8 @@
 @file:Suppress("unused")
 package org.jetbrains.anko
 
-import android.os.Build
 import android.graphics.drawable.Drawable
+import android.os.Build
 import android.util.TypedValue
 import android.view.View
 import android.widget.ImageView
@@ -84,3 +84,39 @@ var TextView.textSizeDimen: Int
 var ImageView.image: Drawable?
     inline get() = drawable
     inline set(value) = setImageDrawable(value)
+
+var TextView.drawableStart: Drawable?
+    get() = drawables[0]
+    set(value) = setDrawables(value, drawableTop, drawableEnd, drawableBottom)
+
+var TextView.drawableTop: Drawable?
+    get() = drawables[1]
+    set(value) = setDrawables(drawableStart, value, drawableEnd, drawableBottom)
+
+var TextView.drawableEnd: Drawable?
+    get() = drawables[2]
+    set(value) = setDrawables(drawableStart, drawableTop, value, drawableBottom)
+
+var TextView.drawableBottom: Drawable?
+    get() = drawables[3]
+    set(value) = setDrawables(drawableStart, drawableTop, drawableEnd, value)
+
+@Deprecated("Consider replace with drawableStart to better support right-to-left Layout", ReplaceWith("drawableStart"))
+var TextView.drawableLeft: Drawable?
+    get() = compoundDrawables[0]
+    set(value) = setCompoundDrawablesWithIntrinsicBounds(value, drawableTop, drawableRight, drawableBottom)
+
+@Deprecated("Consider replace with drawableEnd to better support right-to-left Layout", ReplaceWith("drawableEnd"))
+var TextView.drawableRight: Drawable?
+    get() = compoundDrawables[2]
+    set(value) = setCompoundDrawablesWithIntrinsicBounds(drawableLeft, drawableTop, value, drawableBottom)
+
+private val TextView.drawables: Array<Drawable?>
+    get() = if (Build.VERSION.SDK_INT >= 17) compoundDrawablesRelative else compoundDrawables
+
+private fun TextView.setDrawables(start: Drawable?, top: Drawable?, end: Drawable?, buttom: Drawable?) {
+    if (Build.VERSION.SDK_INT >= 17)
+        setCompoundDrawablesRelativeWithIntrinsicBounds(start, top, end, buttom)
+    else
+        setCompoundDrawablesWithIntrinsicBounds(start, top, end, buttom)
+}
